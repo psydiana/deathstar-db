841b5f44571bd3bc9268475d815c9a668a4c91df
Diana Vasconcelos <dianinhadesouzav@gmail.com>
2025-11-18 16:22:49 -0300

    Add initial data for software component table

---
+++ data/inserts_software.sql
@@ -0,0 +1,11 @@
+INSERT INTO software_component (sw_id, subsystem_id, version, language, status) VALUES
+('sw_superlaser_os', 'superlaser', '4.2.1', 'C', 'running'),
+('sw_superlaser_target', 'superlaser', '3.7.0', 'Python', 'running'),
+('sw_env_monitor', 'life_support', '1.9.0', 'Rust', 'running'),
+('sw_airmix_ctrl', 'life_support', '2.0.5', 'C', 'running'),
+('sw_nav_main', 'navigation', '3.0.2', 'C++', 'running'),
+('sw_nav_autopilot', 'navigation', '1.4.8', 'C++', 'running'),
+('sw_reactor_ctrl', 'reactor_core', '2.3.4', 'C', 'crashed'),
+('sw_reactor_safety', 'reactor_core', '5.1.2', 'Rust', 'running'),
+('sw_hangar_ops', 'hangar_bay', '1.1.9', 'C#', 'running'),
+('sw_security_scan', 'security_net', '7.4.3', 'Go', 'running');
