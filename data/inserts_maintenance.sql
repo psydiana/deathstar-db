1f63eb24664ca9f1094964eba15179e0fc86a737
Diana Vasconcelos <dianinhadesouzav@gmail.com>
2025-11-18 16:25:22 -0300

    Add initial data to maintenance event table

---
+++ data/inserts_maintenance.sql
@@ -0,0 +1,8 @@
+INSERT INTO maintenance_event (module_id, date, technician, details) VALUES
+('reactor_reg_01', '2025-11-18', 'TK-421', 'Replaced burnt capacitor'),
+('coolant_pump_01', '2025-11-17', 'TK-553', 'Refilled coolant reservoir'),
+('life_airmix_02', '2025-11-16', 'TK-110', 'Lubricated moving components'),
+('nav_gyro_02', '2025-11-15', 'TK-888', 'Recalibrated gyroscope'),
+('security_cam_14', '2025-11-18', 'TK-221', 'Cleaned lens and updated firmware'),
+('hangar_tractor_01', '2025-11-18', 'TK-002', 'Power fluctuation test'),
+('laser_array_01', '2025-11-12', 'TK-751', 'Re-aligned crystal array');
